89e9ff2eae9e6f4450a9873042f4795ec379aa91
Xiaoquan Zhou <xiaoquan.zhou@email.edcc.edu>
2019-09-13 11:32:00 -0700

    mysql to deploy

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 var mysql = require("mysql");
+require('dotenv').config();
 
 var connection = mysql.createConnection({
 
     host:"localhost",
     port:3306,
     user:"root",
-    password:"Zxq@820121",
+    password:process.env.mysql_key,
     database:"burger_db"
 });
 
