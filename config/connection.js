7a00ed01247a4760102d4438a2733cbcddca662f
Xiaoquan Zhou <xiaoquan.zhou@email.edcc.edu>
2019-09-14 16:48:35 -0700

    ready to deploy

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 var mysql = require("mysql");
 require('dotenv').config();
 
-var connection = mysql.createConnection({
-
+var connection;
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection= mysql.createConnection({
     host:"localhost",
     port:3306,
     user:"root",
     password:process.env.mysql_key,
     database:"burger_db"
 });
+};
 
 connection.connect(function(err){
     if(err) {
